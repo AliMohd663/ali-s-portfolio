4c50bec79303523f5524189fa917bd0ed8c8ef29
ALI <ali@mac.local>
2025-04-14 23:58:53 +0300

    some txt

--- src/components/ResumeButton/ResumeButton.jsx
+++ src/components/ResumeButton/ResumeButton.jsx
@@ -4,12 +4,13 @@ import styles from "./ResumeButton.module.css";
 export default function ResumeButton() {
   return (
     <a
-      href="../../../public/my-res.pdf"
-      download
-      className={styles.resumeButton}
-    >
-      Download Resume
-    </a>
+  href="../../../public/my-res.pdf"
+  download="../../../public/my-res.pdf"
+  className={styles.resumeButton}
+>
+  Download Resume
+</a>
+
   );
 }
 
